0aac08be320ea12ab4ec80a05f7b646e869a160d
haisia <cwnsgur13579@gmail.com>
2022-12-28 10:51:38 +0900

    프로젝트 생성

---
+++ src/main/java/prac/lineage2m/lineage2m/Lineage2mApplication.java
@@ -0,0 +1,13 @@
+package prac.lineage2m.lineage2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lineage2mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lineage2mApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prac/lineage2m/lineage2m/Lineage2mApplicationTests.java
@@ -0,0 +1,13 @@
+package prac.lineage2m.lineage2m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lineage2mApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
