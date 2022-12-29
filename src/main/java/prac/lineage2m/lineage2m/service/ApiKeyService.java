41583bf5112b55d9bd8a64d6ae0f62e6f150f0b0
haisia <cwnsgur13579@gmail.com>
2022-12-29 15:11:30 +0900

    삭제

--- src/main/java/prac/lineage2m/lineage2m/service/ApiKeyService.java
+++
@@ -1,9 +0,0 @@
-package prac.lineage2m.lineage2m.service;
-
-import prac.lineage2m.lineage2m.domain.ApiKey;
-
-import java.util.Optional;
-
-public interface ApiKeyService {
-  ApiKey findById(Long l);
-}
