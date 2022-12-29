8225a8959ab83e4864cc9c73588b3fa4093e1152
haisia <cwnsgur13579@gmail.com>
2022-12-29 11:45:59 +0900

    [수정]ItemSearchRepositoryImpl.getItemStocks() 호출 시 결과를 ItemSearchDto 에 매핑하도록 구현했는데 나중에 따로 분리할 것

--- src/main/java/prac/lineage2m/lineage2m/repository/ItemSearchRepositoryImpl.java
+++ src/main/java/prac/lineage2m/lineage2m/repository/ItemSearchRepositoryImpl.java
@@ -1,10 +1,11 @@
 package prac.lineage2m.lineage2m.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
 import prac.lineage2m.lineage2m.dto.ItemDto;
+import prac.lineage2m.lineage2m.dto.ItemSearchDto;
 import reactor.core.publisher.Mono;
 
 import java.io.BufferedReader;
@@ -12,11 +13,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Objects;
+import java.util.List;
 
 /**
  * ref : https://gngsn.tistory.com/154
- *
+ * <p>
  * API 호출 예)
  * https://dev-api.plaync.com/
  * l2m/v1.0/market/items/search
@@ -24,47 +25,77 @@ import java.util.Objects;
  * &sale=false
  * &page=1&size=10
  */
-public class ItemSearchRepositoryImpl implements ItemSearchRepository{
+public class ItemSearchRepositoryImpl implements ItemSearchRepository {
 
   public void getItemStocks() throws IOException {
-    String urlString =  "https://dev-api.plaync.com/l2m/v1.0/market/items/search?search_keyword=%ED%95%B8%EB%93%9C%20%EC%98%A4%EB%B8%8C%20%EC%B9%B4%EB%B8%8C%EB%A6%AC%EC%98%A4&sale=false&page=1&size=10";
+    String urlString = "https://dev-api.plaync.com/l2m/v1.0/market/items/search?search_keyword=%ED%95%B8%EB%93%9C%20%EC%98%A4%EB%B8%8C%20%EC%B9%B4%EB%B8%8C%EB%A6%AC%EC%98%A4&sale=false&page=1&size=10";
     URL url = new URL(urlString);
     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    ObjectMapper mapper = new ObjectMapper();
+
     conn.setRequestMethod("GET");
     conn.setRequestProperty("Content-type", "application/json");
     conn.setRequestProperty("Authorization", "Bearer eyJraWQiOiI2YWFmYzEzZi1hMGJjLTQ1YjYtYTUyMS00YTAyMGUzMTljYWEiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOiI3MERFOTg3NS01OEZCLTQ0RTYtOEQ2MS0yNTMxQTU4REUzQTIifQ.aYZQcKmLGAW0-y-XE6xnOAy0q77w5MQUWPDCpErsHz8P8neb6VagevXAyke9quon7MqTpa5qufjIn8zJl2POGuBx-epQ2qKz-nBixSYyuxExOr8RnFJVROYHOoJ2X9xWsIkIrFi0O3dESSZvWOxEXi2KvFnoBAoKoqf7XA3CGBEjkCsHytbPOilwypE0AXvhaasglzUiYVzeUDyTKdn7h9SedVq-jmnvdzsOs-tCIlUvKesKLg1kFVy7_inipXWHuQrTtAtSVkN4-O2RtG_Pocl2wMHYBrOawxPbttS8ac35kMxzPPp7MkxsW6Krz6SVfGzsHJ0CCBIgHElyDzPIeQ");
 
     BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-    StringBuffer response = new StringBuffer();
+    StringBuilder response = new StringBuilder();
 
     String readLine;
     while ((readLine = in.readLine()) != null) {
       response.append(readLine);
     }
-
     in.close();
-    System.out.println("#############################");
-    System.out.println(response.toString());
-  }
 
-  public ItemDto getItemStocksToItemDto() throws IOException {
+    String responseJson = response.toString();
+    System.out.println(responseJson);
+    ItemSearchDto itemSearchDto = mapper.readValue(responseJson, ItemSearchDto.class);
+    System.out.println("itemSearchDto = " + itemSearchDto);
+  }
+  public List<ItemDto> getItemStocksToItemDto() throws IOException {
+
+
+
     String authKey = "Bearer eyJraWQiOiI2YWFmYzEzZi1hMGJjLTQ1YjYtYTUyMS00YTAyMGUzMTljYWEiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOiI3MERFOTg3NS01OEZCLTQ0RTYtOEQ2MS0yNTMxQTU4REUzQTIifQ.aYZQcKmLGAW0-y-XE6xnOAy0q77w5MQUWPDCpErsHz8P8neb6VagevXAyke9quon7MqTpa5qufjIn8zJl2POGuBx-epQ2qKz-nBixSYyuxExOr8RnFJVROYHOoJ2X9xWsIkIrFi0O3dESSZvWOxEXi2KvFnoBAoKoqf7XA3CGBEjkCsHytbPOilwypE0AXvhaasglzUiYVzeUDyTKdn7h9SedVq-jmnvdzsOs-tCIlUvKesKLg1kFVy7_inipXWHuQrTtAtSVkN4-O2RtG_Pocl2wMHYBrOawxPbttS8ac35kMxzPPp7MkxsW6Krz6SVfGzsHJ0CCBIgHElyDzPIeQ";
-    String urlString =  "https://dev-api.plaync.com/l2m/v1.0/market/items/search?search_keyword=%ED%95%B8%EB%93%9C%20%EC%98%A4%EB%B8%8C%20%EC%B9%B4%EB%B8%8C%EB%A6%AC%EC%98%A4&sale=false&page=1&size=10";
-    URL url = new URL(urlString);
-    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    conn.setRequestMethod("GET");
-    conn.setRequestProperty("Content-type", "application/json");
-    conn.setRequestProperty("Authorization", "Bearer eyJraWQiOiI2YWFmYzEzZi1hMGJjLTQ1YjYtYTUyMS00YTAyMGUzMTljYWEiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOiI3MERFOTg3NS01OEZCLTQ0RTYtOEQ2MS0yNTMxQTU4REUzQTIifQ.aYZQcKmLGAW0-y-XE6xnOAy0q77w5MQUWPDCpErsHz8P8neb6VagevXAyke9quon7MqTpa5qufjIn8zJl2POGuBx-epQ2qKz-nBixSYyuxExOr8RnFJVROYHOoJ2X9xWsIkIrFi0O3dESSZvWOxEXi2KvFnoBAoKoqf7XA3CGBEjkCsHytbPOilwypE0AXvhaasglzUiYVzeUDyTKdn7h9SedVq-jmnvdzsOs-tCIlUvKesKLg1kFVy7_inipXWHuQrTtAtSVkN4-O2RtG_Pocl2wMHYBrOawxPbttS8ac35kMxzPPp7MkxsW6Krz6SVfGzsHJ0CCBIgHElyDzPIeQ");
+    String urlString = "https://dev-api.plaync.com/l2m/v1.0/market/items/search?search_keyword=%ED%95%B8%EB%93%9C%20%EC%98%A4%EB%B8%8C%20%EC%B9%B4%EB%B8%8C%EB%A6%AC%EC%98%A4&sale=false&page=1&size=10";
+    ObjectMapper mapper = new ObjectMapper();
 
     WebClient client = WebClient.builder()
             .baseUrl(urlString)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .defaultHeader(HttpHeaders.AUTHORIZATION, authKey)
+            .defaultHeader("Authorization", authKey)
             .build();
 
-    return client.get().accept(MediaType.APPLICATION_JSON)
+    System.out.println("555555555555555555"+client.head().header("Authorization"));
+
+    Mono<Object> response = client.get()
+            .accept(MediaType.APPLICATION_JSON)
             .retrieve()
-            .toEntity(ItemDto.class)
-            .block().getBody();
+            .bodyToMono(Object.class).log();
+    Object object = response.block();
+
+    ItemSearchDto itemSearchDto = mapper.convertValue(object, ItemSearchDto.class);
+
+    System.out.println("########################################");
+    System.out.println("itemSearchDto = " + itemSearchDto);
+    System.out.println("itemSearchDto.getContents() = " + itemSearchDto.getContents());
+    System.out.println("itemSearchDto.getPagination() = " + itemSearchDto.getPagination());
+    System.out.println(itemSearchDto.getPagination().toString());
+
+    for (ItemDto content : itemSearchDto.getContents()) {
+      System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+      System.out.println(content.toString());
+    }
+
+
+
+
+
+//    return Arrays.stream(objects)
+//            .map(itemDto -> mapper.convertValue(itemDto, ItemDto.class))
+//            .collect(Collectors.toList());
+    return null;
+
+//    return response.block();
   }
+
 }
--- src/test/java/prac/lineage2m/lineage2m/repository/ItemSearchRepositoryImplTest.java
+++ src/test/java/prac/lineage2m/lineage2m/repository/ItemSearchRepositoryImplTest.java
@@ -1,19 +1,29 @@
 package prac.lineage2m.lineage2m.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import prac.lineage2m.lineage2m.dto.ItemDto;
 
 import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 class ItemSearchRepositoryImplTest {
-  ItemSearchRepositoryImpl itemSearchRepository = new ItemSearchRepositoryImpl();
+  private final ItemSearchRepositoryImpl itemSearchRepository = new ItemSearchRepositoryImpl();
+
+
 
   @Test
   void getItemStocks() throws IOException {
     itemSearchRepository.getItemStocks();
+  }
+
+  @Test
+  void DTD받기() throws IOException {
+    List<ItemDto> itemStocksToItemDto = itemSearchRepository.getItemStocksToItemDto();
+    System.out.println(itemStocksToItemDto);
+//        System.out.println(itemStocksToItemDto.get(0).toString());
   }
 
 }
