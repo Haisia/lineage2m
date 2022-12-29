16b424db8d6e1ebe29b66fb10e1e4a865bf29c0d
haisia <cwnsgur13579@gmail.com>
2022-12-29 13:29:11 +0900

    테스트코드 업데이트

--- src/test/java/prac/lineage2m/lineage2m/repository/ItemSearchRepositoryImplTest.java
+++ src/test/java/prac/lineage2m/lineage2m/repository/ItemSearchRepositoryImplTest.java
@@ -1,29 +1,57 @@
 package prac.lineage2m.lineage2m.repository;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import prac.lineage2m.lineage2m.dto.ItemDto;
+import prac.lineage2m.lineage2m.dto.itemStockSearch.SearchParamDto;
 
 import java.io.IOException;
-import java.util.List;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 class ItemSearchRepositoryImplTest {
-  private final ItemSearchRepositoryImpl itemSearchRepository = new ItemSearchRepositoryImpl();
+  private final ItemStockSearchRepositoryImpl itemSearchRepository = new ItemStockSearchRepositoryImpl();
 
+  // 이하 getItemStocksToString() 테스트
+  @Test
+  void getItemStocks() throws IOException, IllegalAccessException {
+    String search_keyword = URLEncoder.encode("핸드", StandardCharsets.UTF_8);
+    SearchParamDto searchParamDto = SearchParamDto.builder()
+            .search_keyword(search_keyword)
+            .from_enchant_level(1L)
+            .to_enchant_level(1L)
+            .server_id(1L)
+            .sale(false)
+            .page(1L)
+            .size(30L)
+            .build();
 
+    System.out.println(itemSearchRepository.getItemStocksToJsonString(searchParamDto));
+  }
+
+  // 이하 getItemStocksToObject() 테스트
 
   @Test
-  void getItemStocks() throws IOException {
-    itemSearchRepository.getItemStocks();
+  void getItemStocksToObject() {
+
   }
 
   @Test
-  void DTD받기() throws IOException {
-    List<ItemDto> itemStocksToItemDto = itemSearchRepository.getItemStocksToItemDto();
-    System.out.println(itemStocksToItemDto);
-//        System.out.println(itemStocksToItemDto.get(0).toString());
+  void getUriFromSearchParamDto() throws IllegalAccessException {
+    String search_keyword = URLEncoder.encode("핸드", StandardCharsets.UTF_8);
+    SearchParamDto searchParamDto = SearchParamDto.builder()
+            .search_keyword(search_keyword)
+            .from_enchant_level(1L)
+            .to_enchant_level(1L)
+            .server_id(1L)
+            .sale(false)
+            .page(1L)
+            .size(30L)
+            .build();
+
+    System.out.println(itemSearchRepository.getUriFromDto(searchParamDto));
+
   }
+
 
 }
