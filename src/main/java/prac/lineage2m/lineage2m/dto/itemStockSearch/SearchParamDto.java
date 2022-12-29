de2d069425b9ef6349cb7e4c899d8a249d003b38
haisia <cwnsgur13579@gmail.com>
2022-12-29 13:22:36 +0900

    [추가] 검색 파라미터를 전달하는 DTO 생성 - SearchParamDto

---
+++ src/main/java/prac/lineage2m/lineage2m/dto/itemStockSearch/SearchParamDto.java
@@ -0,0 +1,27 @@
+package prac.lineage2m.lineage2m.dto.itemStockSearch;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class SearchParamDto {
+  private String search_keyword;
+  private Long from_enchant_level;
+  private Long to_enchant_level;
+  private Long server_id;
+  private boolean sale;
+  private Long page;
+  private Long size;
+
+  @Builder
+  public SearchParamDto(String search_keyword, Long from_enchant_level, Long to_enchant_level, Long server_id, boolean sale, Long page, Long size) {
+    this.search_keyword = search_keyword;
+    this.from_enchant_level = from_enchant_level;
+    this.to_enchant_level = to_enchant_level;
+    this.server_id = server_id;
+    this.sale = sale;
+    this.page = page;
+    this.size = size;
+  }
+}
