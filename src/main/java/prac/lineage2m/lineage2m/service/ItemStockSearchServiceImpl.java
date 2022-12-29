a4859b77e4a0da776ff47d6f2dc1d97d59884c51
haisia <cwnsgur13579@gmail.com>
2022-12-29 17:08:54 +0900

    [수정] 입력값중 String 이 URL 인코딩이 안된상태라고 가정하고 서비스에서 인코딩해준다. 추후 프론트설계 시 맞춰서 수정하자

--- src/main/java/prac/lineage2m/lineage2m/service/ItemStockSearchServiceImpl.java
+++ src/main/java/prac/lineage2m/lineage2m/service/ItemStockSearchServiceImpl.java
@@ -8,6 +8,8 @@ import prac.lineage2m.lineage2m.repository.ApiKeyRepository;
 import prac.lineage2m.lineage2m.repository.ItemStockSearchRepository;
 import prac.lineage2m.lineage2m.util.GlobalUtil;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
@@ -21,9 +23,9 @@ public class ItemStockSearchServiceImpl implements ItemStockSearchService{
     // TODO : 임시로 첫번째 키 사용하는데 나중에 상황에 맞춰서 사용할 키값을 자동으로 선택하도록 바꾸자
     String key = GlobalUtil.keyMaker(keyList.get(0));
 
+    // todo : String 이 url 인코딩이 안되어있다고 가정하고 진행한다. 나중에 프론트 만들때 인코딩 할지 말지 정하자
+    searchParamDto.setSearch_keyword(URLEncoder.encode(searchParamDto.getSearch_keyword(), StandardCharsets.UTF_8));
+
     return itemStockSearchRepository.getItemStocksToObject(searchParamDto, key);
   }
-
-
-
 }
