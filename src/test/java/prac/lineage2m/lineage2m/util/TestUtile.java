8bd865dfa93b20ba5cd0a44dd64a68e6277762eb
haisia <cwnsgur13579@gmail.com>
2022-12-30 11:59:53 +0900

    [수정] itemMaker 에 테스트 케이스 추가

--- src/test/java/prac/lineage2m/lineage2m/util/TestUtile.java
+++ src/test/java/prac/lineage2m/lineage2m/util/TestUtile.java
@@ -21,6 +21,8 @@ public class TestUtile {
 
   public static List<Map<String,String>> itemMaker(){
     List<Map<String,String>> list = new ArrayList<>();
+
+    // 아무조건 안넣으면 모든 아이템이 검색대상이 됨.
     list.add(new HashMap<String,String>(){
       {
         put("search_keyword","");
@@ -33,126 +35,145 @@ public class TestUtile {
       }
     });
 
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
-//
-//    list.add(new HashMap<String,String>(){
-//      {
-//        put("search_keyword","");
-//        put("sale","");
-//        put("from_enchant_level","");
-//        put("to_enchant_level","");
-//        put("server_id","");
-//        put("page","");
-//        put("size","");
-//      }
-//    });
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","핸드");
+        put("sale","");
+        put("from_enchant_level","");
+        put("to_enchant_level","");
+        put("server_id","");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","이런이름의아이템은없습니다");
+        put("sale","");
+        put("from_enchant_level","");
+        put("to_enchant_level","");
+        put("server_id","");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    // 5강 이상인 모든아이템
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","발라카스");
+        put("sale","");
+        put("from_enchant_level","5");
+        put("to_enchant_level","");
+        put("server_id","");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    // 3강인 모든아이템
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","안타라스");
+        put("sale","");
+        put("from_enchant_level","3");
+        put("to_enchant_level","3");
+        put("server_id","");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    // 아이템 이름에 '핸드' 가 포함되고 2강인 아이템
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","핸드");
+        put("sale","");
+        put("from_enchant_level","2");
+        put("to_enchant_level","2");
+        put("server_id","");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","");
+        put("sale","true");
+        put("from_enchant_level","8");
+        put("to_enchant_level","");
+        put("server_id","15");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","");
+        put("sale","");
+        put("from_enchant_level","");
+        put("to_enchant_level","");
+        put("server_id","5");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","");
+        put("sale","");
+        put("from_enchant_level","");
+        put("to_enchant_level","");
+        put("server_id","125");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","");
+        put("sale","");
+        put("from_enchant_level","");
+        put("to_enchant_level","");
+        put("server_id","135");
+        put("page","");
+        put("size","");
+      }
+    });
+
+    list.add(new HashMap<String,String>(){
+      {
+        put("search_keyword","");
+        put("sale","");
+        put("from_enchant_level","15");
+        put("to_enchant_level","");
+        put("server_id","");
+        put("page","");
+        put("size","");
+      }
+    });
 
     return list;
   }
 }
+
+
+
+
+
+//    list.add(new HashMap<String,String>(){
+//      {
+//        put("search_keyword","");
+//        put("sale","");
+//        put("from_enchant_level","");
+//        put("to_enchant_level","");
+//        put("server_id","");
+//        put("page","");
+//        put("size","");
+//      }
+//    });
