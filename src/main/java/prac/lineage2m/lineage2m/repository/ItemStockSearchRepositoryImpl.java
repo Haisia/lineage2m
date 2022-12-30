dd47fbe550c0e946060d6f792d1b3132935fe38f
haisia <cwnsgur13579@gmail.com>
2022-12-30 13:02:24 +0900

    [수정] 재사용 할 것 같은 메소드 GlobalUtil 로 분리 - getUriFromDto - jsonToObjectMapping

--- src/main/java/prac/lineage2m/lineage2m/repository/ItemStockSearchRepositoryImpl.java
+++ src/main/java/prac/lineage2m/lineage2m/repository/ItemStockSearchRepositoryImpl.java
@@ -1,7 +1,5 @@
 package prac.lineage2m.lineage2m.repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import prac.lineage2m.lineage2m.dto.itemStockSearch.ItemSearchDto;
 import prac.lineage2m.lineage2m.dto.itemStockSearch.SearchParamDto;
@@ -9,10 +7,12 @@ import prac.lineage2m.lineage2m.dto.itemStockSearch.SearchParamDto;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
+import static prac.lineage2m.lineage2m.util.GlobalUtil.getUriFromDto;
+import static prac.lineage2m.lineage2m.util.GlobalUtil.jsonToObjectMapping;
 
 /**
  * ref : https://gngsn.tistory.com/154
@@ -69,43 +69,4 @@ public class ItemStockSearchRepositoryImpl implements ItemStockSearchRepository 
     return jsonToObjectMapping(json, new ItemSearchDto());
   }
 
-  /**
-   * String 타입 Json 형식의 문자열 arg1 을 arg2 객체에 매핑시켜 주는 메소드.
-   *
-   * @param json   json of String type
-   * @param target new className()
-   * @return Mapped Object
-   */
-  public <T> T jsonToObjectMapping(String json, T target) {
-    ObjectMapper mapper = new ObjectMapper();
-    T result = null;
-    try {
-      result = (T) mapper.readValue(json, target.getClass());
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
-    }
-
-    return result;
-  }
-
-  /**
-   * DTO 를 파싱해서 쿼리스트링 형태의 문자열로 만들어주는 메소드.
-   *
-   * @param dto
-   * @param <T>
-   * @return dto의 파라미터로 부터 파싱한 queryString 형태의 String 타입
-   * @throws IllegalAccessException
-   */
-  public <T> String getUriFromDto(T dto) throws IllegalAccessException {
-    Class<?> clazz = dto.getClass();
-    Field[] declaredFields = clazz.getDeclaredFields();
-    StringBuilder uri = new StringBuilder();
-    for (Field declaredField : declaredFields) {
-      declaredField.setAccessible(true);
-      if (declaredField.get(dto) != null) {
-        uri.append("&").append(declaredField.getName()).append("=").append(declaredField.get(dto));
-      }
-    }
-    return uri.toString();
-  }
 }
