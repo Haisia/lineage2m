6bcfe53f786a0fb449fa4df7ef2e5a797f26ba89
haisia <cwnsgur13579@gmail.com>
2022-12-30 15:48:54 +0900

    [추가] GlobalUtil.getUriFromDto 는 Dto 의 모든 필드를 순회해서 Uri 화 시키기 때문에 중간에 pathVariable 로 받는 item_id 필드가 없는 새로운 ParamDto 가 필요해서 만듦.

---
+++ src/main/java/prac/lineage2m/lineage2m/dto/itemPriceStatsSearch/ParamDtoForRepository.java
@@ -0,0 +1,25 @@
+package prac.lineage2m.lineage2m.dto.itemPriceStatsSearch;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * GlobalUtil.getUriFromDto 는
+ * Dto 의 모든 필드를 순회해서 Uri 화 시키기 때문에
+ * 중간에 pathVariable 로 받는 item_id 필드가 없는
+ * 새로운 ParamDto 가 필요해서 만듦.
+ */
+@Getter @Setter
+@ToString
+public class ParamDtoForRepository {
+  private Long server_id;
+  private Long enchant_level;
+
+  @Builder
+  public ParamDtoForRepository(Long server_id, Long enchant_level) {
+    this.server_id = server_id;
+    this.enchant_level = enchant_level;
+  }
+}
