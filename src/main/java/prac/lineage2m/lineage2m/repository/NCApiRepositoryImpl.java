18c00bb9cb9bd55e8a59038611af04df2d11d983
haisia <cwnsgur13579@gmail.com>
2023-01-02 15:24:52 +0900

    [수정] Util 메소드 위치 변경

--- src/main/java/prac/lineage2m/lineage2m/repository/NCApiRepositoryImpl.java
+++ src/main/java/prac/lineage2m/lineage2m/repository/NCApiRepositoryImpl.java
@@ -1,5 +1,7 @@
 package prac.lineage2m.lineage2m.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import prac.lineage2m.lineage2m.dto.itemInfoSearch.InfoParamDto;
 import prac.lineage2m.lineage2m.dto.itemInfoSearch.InfoParamForRepositoryDto;
@@ -11,13 +13,11 @@ import prac.lineage2m.lineage2m.dto.itemStockSearch.StockParamDto;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-
-import static prac.lineage2m.lineage2m.util.GlobalUtil.getUriFromDto;
-import static prac.lineage2m.lineage2m.util.GlobalUtil.jsonToObjectMapping;
 
 /**
  * ref : https://gngsn.tistory.com/154
@@ -97,5 +97,47 @@ public class NCApiRepositoryImpl implements NCApiRepository {
       throw new RuntimeException(e);
     }
     return conn;
+  }
+
+  /**
+   * String 타입 Json 형식의 문자열 arg1 을 arg2 객체에 매핑시켜 주는 메소드.
+   * @param json   json of String type
+   * @param target new className()
+   * @return Mapped Object
+   */
+  public static <T> T jsonToObjectMapping(String json, T target) {
+    ObjectMapper mapper = new ObjectMapper();
+    T result = null;
+    try {
+      result = (T) mapper.readValue(json, target.getClass());
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+    return result;
+  }
+
+  /**
+   * DTO 를 파싱해서 쿼리스트링 형태의 문자열로 만들어주는 메소드.
+   * @param dto
+   * @param <T>
+   * @return dto의 파라미터로 부터 파싱한 queryString 형태의 String 타입
+   * @throws IllegalAccessException
+   */
+  public static <T> String getUriFromDto(T dto) throws IllegalAccessException {
+    Class<?> clazz = dto.getClass();
+    Field[] declaredFields = clazz.getDeclaredFields();
+    StringBuilder uri = new StringBuilder();
+    boolean isFirstField = true;
+
+    for (Field declaredField : declaredFields) {
+      declaredField.setAccessible(true);
+      if (declaredField.get(dto) != null && isFirstField) {
+        uri.append(declaredField.getName()).append("=").append(declaredField.get(dto));
+        isFirstField=false;
+      } else if (declaredField.get(dto) != null) {
+        uri.append("&").append(declaredField.getName()).append("=").append(declaredField.get(dto));
+      }
+    }
+    return uri.toString();
   }
 }
--- src/main/java/prac/lineage2m/lineage2m/util/GlobalUtil.java
+++ src/main/java/prac/lineage2m/lineage2m/util/GlobalUtil.java
@@ -10,45 +10,5 @@ public class GlobalUtil {
     return "Bearer " + key;
   }
 
-  /**
-   * String 타입 Json 형식의 문자열 arg1 을 arg2 객체에 매핑시켜 주는 메소드.
-   * @param json   json of String type
-   * @param target new className()
-   * @return Mapped Object
-   */
-  public static <T> T jsonToObjectMapping(String json, T target) {
-    ObjectMapper mapper = new ObjectMapper();
-    T result = null;
-    try {
-      result = (T) mapper.readValue(json, target.getClass());
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
-    }
-    return result;
-  }
 
-  /**
-   * DTO 를 파싱해서 쿼리스트링 형태의 문자열로 만들어주는 메소드.
-   * @param dto
-   * @param <T>
-   * @return dto의 파라미터로 부터 파싱한 queryString 형태의 String 타입
-   * @throws IllegalAccessException
-   */
-  public static <T> String getUriFromDto(T dto) throws IllegalAccessException {
-    Class<?> clazz = dto.getClass();
-    Field[] declaredFields = clazz.getDeclaredFields();
-    StringBuilder uri = new StringBuilder();
-    boolean isFirstField = true;
-
-    for (Field declaredField : declaredFields) {
-      declaredField.setAccessible(true);
-      if (declaredField.get(dto) != null && isFirstField) {
-        uri.append(declaredField.getName()).append("=").append(declaredField.get(dto));
-        isFirstField=false;
-      } else if (declaredField.get(dto) != null) {
-        uri.append("&").append(declaredField.getName()).append("=").append(declaredField.get(dto));
-      }
-    }
-    return uri.toString();
-  }
 }
