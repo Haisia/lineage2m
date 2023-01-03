48dc46bf99ccc361080400635d5c9c8398f481f1
haisia <cwnsgur13579@gmail.com>
2023-01-03 17:19:23 +0900

    [추가] Server, World 목록 받아온거 DB에 추가하기 전에 이미 들어있는지 확인하는 로직 추가. 이미 있으면 추가안함 (NC API 문제로 world_id 가 null 로 오는 경우가 있어서 임시로 findByName 으로 존재여부 확인했음. 나중에 NC 에서 고치면 findByWorldId 로 바꾸는게 좋을 듯.)

--- src/main/java/prac/lineage2m/lineage2m/service/AdminServiceImpl.java
+++ src/main/java/prac/lineage2m/lineage2m/service/AdminServiceImpl.java
@@ -6,7 +6,9 @@ import prac.lineage2m.lineage2m.dto.ServerListSearch.ServerListResultDto;
 import prac.lineage2m.lineage2m.dto.ServerListSearch.ServerListServerDto;
 import prac.lineage2m.lineage2m.entity.Server;
 import prac.lineage2m.lineage2m.entity.World;
+import prac.lineage2m.lineage2m.repository.ServerCustomRepository;
 import prac.lineage2m.lineage2m.repository.ServerRepository;
+import prac.lineage2m.lineage2m.repository.WorldCustomRepository;
 import prac.lineage2m.lineage2m.repository.WorldRepository;
 
 import java.util.*;
@@ -17,11 +19,12 @@ public class AdminServiceImpl implements AdminService{
   private final ServerListService serverListService;
   private final WorldRepository worldRepository;
   private final ServerRepository serverRepository;
+  private final WorldCustomRepository worldCustomRepository;
+  private final ServerCustomRepository serverCustomRepository;
 
   /**
    * API 로 월드랑 서버목록 전체 받아와서 DB에 등록한다.
    * 서버가 추가됐을 때만 한번씩 돌려주면 된다.
-   * TODO : DB에 존재하는지 확인하고 없을때만 추가하는 조건 추가하면 좋을 듯
    * @return 성공여부
    */
   public boolean saveWorldAndServerList() {
@@ -41,11 +44,9 @@ public class AdminServiceImpl implements AdminService{
       }
     }
 
-    worldRepository.saveAllAndFlush(worldList);
-    worldList = worldRepository.findAll();
     Map<String, Long> worldNameAndPk = new HashMap<>();
-
     for (World world : worldList) {
+      if(worldCustomRepository.findByWorldName(world.getWorldName()).isEmpty()) worldRepository.saveAndFlush(world);
       worldNameAndPk.put(world.getWorldName(),world.getPk());
     }
 
@@ -54,12 +55,20 @@ public class AdminServiceImpl implements AdminService{
       serverName = serverName.substring(0,serverName.length()-2);
 
       Long worldPk = worldNameAndPk.get(serverName);
-      World world = worldRepository.findById(worldPk).orElseThrow();
+
+      World world = null;
+      try {
+        world = worldRepository.findById(worldPk).orElseThrow();
+      } catch (Exception e) {
+        return false;
+      }
 
       server.setWorld(world);
-    }
 
-    serverRepository.saveAllAndFlush(serverList);
+      if(serverCustomRepository.findByServerName(server.getServerName()).isEmpty()) {
+        serverRepository.saveAndFlush(server);
+      }
+    }
 
     return true;
   }
