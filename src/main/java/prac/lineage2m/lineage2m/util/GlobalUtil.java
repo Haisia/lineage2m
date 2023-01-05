48e3a0a20ffec4d23d9ef5893cf815e87bbe0665
haisia <cwnsgur13579@gmail.com>
2023-01-05 13:17:43 +0900

    [추가] 동일한 필드타입, 필드명을 기준으로 필드값 복사해주는 유틸 추가 (DTO <-> Entity 간의 변경이 필요할 때 사용하자.)

--- src/main/java/prac/lineage2m/lineage2m/util/GlobalUtil.java
+++ src/main/java/prac/lineage2m/lineage2m/util/GlobalUtil.java
@@ -1,8 +1,5 @@
 package prac.lineage2m.lineage2m.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.lang.reflect.Field;
 
 public class GlobalUtil {
@@ -10,5 +7,33 @@ public class GlobalUtil {
     return "Bearer " + key;
   }
 
+  /**
+   * source 객체를 result 객체로 변환해주는 유틸.
+   * 사용조건
+   * 1. source 와 result 의 모든 필드의 타입과 필드명이 동일해야 한다.
+   * 2. result 의 모든 필드가 source 에 포함되어 있어야 한다. (아닐 시 해당 값 null)
+   * @return 변환된 객체
+   */
+  public static <S, R> R convertObjectBySameField(S source, R result) {
+    Field[] sourceFields = source.getClass().getDeclaredFields();
+    Class<?> resultClass = result.getClass();
+
+    for (Field sourceField : sourceFields) {
+      Field resultField = null;
+      try {
+        resultField = resultClass.getDeclaredField(sourceField.getName());
+      } catch (NoSuchFieldException | SecurityException e) {
+        continue;
+      }
+      sourceField.setAccessible(true);
+      resultField.setAccessible(true);
+      try {
+        resultField.set(result, sourceField.get(source));
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return result;
+  }
 
 }
