65eab258d438e1053a84ef8990a1510eb0d03651
haisia <cwnsgur13579@gmail.com>
2023-01-06 16:18:09 +0900

    [BE - 추가] updateItemList - ItemList를 토대로 모든 아이템 정보를 DB에 추가한다. 아이템 이름으로 거래소에 검색하고 결과로 받은 item_id 로 itemInfoSearch 를 한다. NC가 아이템검색은 아이템 이름으로 검색을 지원하지 않고, itemId 목록을 알아낼 방법이 없어서 어쩔 수 없이 일단 이렇게 했다.

--- src/main/java/prac/lineage2m/lineage2m/service/AdminService.java
+++ src/main/java/prac/lineage2m/lineage2m/service/AdminService.java
@@ -2,4 +2,5 @@ package prac.lineage2m.lineage2m.service;
 
 public interface AdminService {
   boolean saveWorldAndServerList();
+  boolean updateItemList();
 }
--- src/main/java/prac/lineage2m/lineage2m/service/AdminServiceImpl.java
+++ src/main/java/prac/lineage2m/lineage2m/service/AdminServiceImpl.java
@@ -4,23 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import prac.lineage2m.lineage2m.dto.ServerListSearch.ServerListResultDto;
 import prac.lineage2m.lineage2m.dto.ServerListSearch.ServerListServerDto;
+import prac.lineage2m.lineage2m.dto.itemInfoSearch.InfoParamDto;
+import prac.lineage2m.lineage2m.dto.itemStockSearch.StockContentsDto;
+import prac.lineage2m.lineage2m.dto.itemStockSearch.StockParamDto;
+import prac.lineage2m.lineage2m.dto.itemStockSearch.StockResultDto;
 import prac.lineage2m.lineage2m.entity.Server;
 import prac.lineage2m.lineage2m.entity.World;
 import prac.lineage2m.lineage2m.repository.server.ServerRepository;
 import prac.lineage2m.lineage2m.repository.world.WorldRepository;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
-public class AdminServiceImpl implements AdminService{
+public class AdminServiceImpl implements AdminService {
+  private final static int MAX_ENCHANT_LEVEL = 13;
   private final ServerListService serverListService;
   private final WorldRepository worldRepository;
   private final ServerRepository serverRepository;
+  private final ItemStockSearchService itemStockSearchService;
+  private final ItemInfoSearchService itemInfoSearchService;
 
   /**
    * API 로 월드랑 서버목록 전체 받아와서 DB에 등록한다.
    * 신서버 나올때만 한번씩 돌려주면 된다.
+   *
    * @return 성공여부
    */
   public boolean saveWorldAndServerList() {
@@ -36,10 +47,44 @@ public class AdminServiceImpl implements AdminService{
     return saveDistinctServer(serverList, worldNameAndPk);
   }
 
+  /**
+   * 아이템이름으로 거래소에 검색하고 결과로 받은 item_id 로 itemInfoSearch 를 한다.
+   * NC가 아이템검색은 아이템 이름으로 검색을 지원하지 않고,
+   * itemId 목록을 알아낼 방법이 없어서
+   * 어쩔 수 없이 일단 이렇게 했다.
+   * @return 성공 여부
+   */
+  public boolean updateItemList() {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ishift\\Desktop\\lineage2m\\documents\\ItemList", StandardCharsets.UTF_8));
+      String itemName;
+      while ((itemName = br.readLine()) != null) {
+        if (!itemName.startsWith("<") && itemName.length() != 0) {
+          List<StockContentsDto> contents = itemStockSearchService.getItemStocksToObject(StockParamDto.builder()
+                  .search_keyword(itemName)
+                  .sale(false)
+                  .build()).getContents();
+
+          if (contents.size() > 0) {
+            Long itemId = contents.get(0).getItemId();
+            for (int i = 0; i <= MAX_ENCHANT_LEVEL; i++) {
+              itemInfoSearchService.getItemInfoToObject(InfoParamDto.builder().item_id(itemId).enchant_level((long) i).build());
+            }
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+
+    return true;
+  }
+
   private boolean saveDistinctServer(List<Server> serverList, Map<String, Long> worldNameAndPk) {
     for (Server server : serverList) {
       String serverName = server.getServerName();
-      serverName = serverName.substring(0,serverName.length()-2);
+      serverName = serverName.substring(0, serverName.length() - 2);
 
       Long worldPk = worldNameAndPk.get(serverName);
 
@@ -52,7 +97,7 @@ public class AdminServiceImpl implements AdminService{
 
       server.setWorld(world);
 
-      if(serverRepository.findByServerName(server.getServerName()).isEmpty()) {
+      if (serverRepository.findByServerName(server.getServerName()).isEmpty()) {
         serverRepository.saveAndFlush(server);
       }
     }
@@ -62,8 +107,8 @@ public class AdminServiceImpl implements AdminService{
   private Map<String, Long> saveDistinctWorld(List<World> worldList) {
     Map<String, Long> worldNameAndPk = new HashMap<>();
     for (World world : worldList) {
-      if(worldRepository.findByWorldName(world.getWorldName()).isEmpty()) worldRepository.saveAndFlush(world);
-      worldNameAndPk.put(world.getWorldName(),world.getPk());
+      if (worldRepository.findByWorldName(world.getWorldName()).isEmpty()) worldRepository.saveAndFlush(world);
+      worldNameAndPk.put(world.getWorldName(), world.getPk());
     }
     return worldNameAndPk;
   }
@@ -75,7 +120,7 @@ public class AdminServiceImpl implements AdminService{
 
       List<ServerListServerDto> serverListOfDto = worldDto.getServers();
       for (ServerListServerDto serverDto : serverListOfDto) {
-        Server server = new Server(serverDto.getServerId(),serverDto.getServerName());
+        Server server = new Server(serverDto.getServerId(), serverDto.getServerName());
         serverList.add(server);
       }
     }
