4210e7772d258f6b99914fe5466bd356bddc9fda
haisia <cwnsgur13579@gmail.com>
2023-01-11 11:22:47 +0900

    [BE - 수정] 동일호스트, 동일포트가 아니면 Cross뭐시기 에러뜬다... 임시로 프론트에서 사용하는 3000번 포트를 허용해줘서 해결했다. 나중에 더 좋은 방법을 찾아보자.

--- src/main/java/prac/lineage2m/lineage2m/controller/AdminController.java
+++ src/main/java/prac/lineage2m/lineage2m/controller/AdminController.java
@@ -1,18 +1,16 @@
 package prac.lineage2m.lineage2m.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import prac.lineage2m.lineage2m.entity.ItemInfo;
-import prac.lineage2m.lineage2m.entity.ItemOption;
-import prac.lineage2m.lineage2m.repository.ItemInfoRepository;
-import prac.lineage2m.lineage2m.repository.ItemOptionRepository;
 import prac.lineage2m.lineage2m.service.AdminService;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
